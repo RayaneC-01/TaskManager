433b709c2a44bf09dcf327e97685fac3ae7776ef
Rayane <rchaabane20@gmail.com>
2024-03-14 20:24:04 +0100

    Base de donnée

--- task_management_app.sql
+++ task_management_app.sql
@@ -1,11 +1,8 @@
 -- phpMyAdmin SQL Dump
 -- version 5.2.0
--- https://www.phpmyadmin.net/
---
 -- Hôte : 127.0.0.1:3306
 -- Généré le : mer. 13 mars 2024 à 20:29
 -- Version du serveur : 8.0.31
--- Version de PHP : 8.0.26
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
