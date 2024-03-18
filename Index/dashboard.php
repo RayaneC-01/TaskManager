09825fc3ae5e9f4a256d47cf6e522b804baaeb95
Rayane <rchaabane20@gmail.com>
2024-03-18 18:47:10 +0100

    Tableau de Bord

--- Index/dashboard.php
+++ Index/dashboard.php
@@ -2,17 +2,16 @@
 // Connectez-vous à votre base de données
 require_once '../config/connexion_database.php';
 
-//démarrer la session
+// Démarrer la session
 session_start();
 
 // Vérifiez si l'utilisateur est connecté
-if (isset ($_SESSION['identifier'])) {
+if (isset ($_SESSION['username'])) {
     // Récupérez l'identifiant de l'utilisateur depuis la session
-    $username = $_SESSION['identifier'];
+    $username = $_SESSION['username'];
     $message = $username;
 
-
-    // Préparez et exécutez une requête SQL pour sélectionner le prénom de l'utilisateur
+    // Préparez et exécutez une requête SQL pour sélectionner les informations de l'utilisateur
     $stmt = $conn->prepare("SELECT first_name, last_name, email, created_at, last_connexion FROM users WHERE username = :username");
     $stmt->bindParam(':username', $username);
     $stmt->execute();
@@ -22,7 +21,7 @@ if (isset ($_SESSION['identifier'])) {
 
     // Vérifiez si le résultat est non vide
     if ($row) {
-        // Récupérez le prénom de l'utilisateur
+        // Récupérez les informations de l'utilisateur
         $first_name = $row['first_name'];
         $last_name = $row['last_name'];
         $email = $row['email'];
@@ -39,15 +38,13 @@ if (isset ($_SESSION['identifier'])) {
 }
 ?>
 
-
 <?php
 $pageTitle = "Tableau de Bord Gestionnaire de tâches";
-require_once 'header.php'; ?>
+require_once 'header.php';
+?>
 
 <body>
-
     <div class="container">
-
         <p class="h3">
             Bonjour, <strong>
                 <?php echo $message ?>
@@ -57,22 +54,20 @@ require_once 'header.php'; ?>
             <div class="card">
                 <div class="card-body">
                     <ul class="list-group list-group-flush">
-                        <li class="list-group-item h4">Nom:
-                            <strong>
-                                <?php echo $last_name;
-                                ?>
-                            </strong>
-                        </li>
-                        <li class=" list-group-item h4">Prénom: <strong>
+                        <li class="list-group-item h4">Nom: <strong>
+                                <?php echo $last_name; ?>
+                            </strong></li>
+                        <li class="list-group-item h4">Prénom: <strong>
                                 <?php echo $first_name; ?>
-                            </strong>
-                        </li>
+                            </strong></li>
                         <li class="list-group-item h4">Email: <strong>
                                 <?php echo $email; ?>
                             </strong></li>
-                        <li class="list-group-item h4">Date de création du compte: <strong>
+                        <li class="list-group-item h4">Date de création du compte:
+                            <strong>
                                 <?php echo $created_at; ?>
-                            </strong></li>
+                            </strong>
+                        </li>
                         <li class="list-group-item h4">Dernière Connexion est le:
                             <strong>
                                 <?php echo $last_connexion; ?>
@@ -83,7 +78,4 @@ require_once 'header.php'; ?>
             </div>
         </div>
     </div>
-
-
-
 </body>
