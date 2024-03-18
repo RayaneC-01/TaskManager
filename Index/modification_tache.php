492612fcecf825dcd93f7a633abdd3a7e0f03fad
Rayane <rchaabane20@gmail.com>
2024-03-18 18:52:30 +0100

    modifier une tache

--- Index/modification_tache.php
+++ Index/modification_tache.php
@@ -1,95 +1,99 @@
 <?php
-// Vérifier si l'utilisateur est connecté
 session_start();
-if (!isset ($_SESSION['utilisateur_connecte']) || !$_SESSION['utilisateur_connecte']) {
+
+// Vérifier si l'utilisateur est connecté
+if (!isset ($_SESSION['identifier'])) {
     // Rediriger vers la page de connexion si l'utilisateur n'est pas connecté
     header('Location: connexion.php');
     exit;
 }
 
-// Vérifier si l'ID de la tâche à modifier est présent dans les données POST
-if (!isset ($_POST['id'])) {
-    // Rediriger vers la page d'accueil si aucun ID n'est fourni
-    header('Location: accueil.php');
-    exit;
-}
+// Récupérer l'identifiant de l'utilisateur connecté
+$user_id = $_SESSION['utilisateur_connecte'];
 
-// Récupérer l'ID de la tâche depuis les données POST
-$id_tache = $_POST['id'];
-
-// Connexion à la base de données
 require_once '../config/connexion_database.php';
 
-try {
-    // Préparer la requête pour récupérer les détails de la tâche à partir de son ID
-    $stmt = $conn->prepare("SELECT * FROM tasks WHERE id = :id");
-    $stmt->bindParam(':id', $id_tache);
-    $stmt->execute();
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset ($_POST['title'])) {
+    // Validation et nettoyage du titre de la tâche
+    $titre = htmlspecialchars(trim($_POST['title']));
 
-    // Vérifier si la tâche existe
-    if ($stmt->rowCount() > 0) {
-        // Récupérer les détails de la tâche
-        $tache = $stmt->fetch();
-    } else {
-        // Rediriger vers la page d'accueil si la tâche n'est pas trouvée
+    // Vérifier si le titre est vide après le nettoyage
+    if (empty ($titre)) {
+        $_SESSION['message_error'] = "Erreur : Le titre de la tâche est requis.";
         header('Location: accueil.php');
         exit;
     }
-} catch (PDOException $e) {
-    // En cas d'erreur, afficher un message d'erreur
-    $_SESSION['message_error'] = "Erreur lors de la récupération des détails de la tâche : " . $e->getMessage();
-    // Rediriger vers la page d'accueil
-    header('Location: accueil.php');
-    exit;
-}
 
-// Vérifier si des données de formulaire ont été soumises pour la mise à jour de la tâche
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset ($_POST['modifier_tache'])) {
+    $due_date = null;
+    $priority = $_POST['priority']; // Récupération de la priorité depuis le formulaire
+
+    // priorité est un entier
+    $priority = intval($priority);
+
+    // Vérifier si l'utilisateur a choisi une date personnalisée
+    if ($_POST['due_date'] === 'choose_date') {
+        // Récupérer la date personnalisée à partir du champ de formulaire
+        $due_date = $_POST['custom_due_date'];
+
+        // Vérifier si aucune date n'a été sélectionnée
+        if (empty ($due_date)) {
+            // Afficher un message d'erreur et rediriger vers la page d'accueil
+            $_SESSION['message_error'] = "Erreur : Une date d'échéance est requise.";
+            header('Location: accueil.php');
+            exit;
+        }
+    } else {
+        // Traitement de la date en fonction de l'option choisie
+        switch ($_POST['due_date']) {
+            case 'today':
+                $due_date = date('Y-m-d');
+                break;
+            case 'tomorrow':
+                $due_date = date('Y-m-d', strtotime('+1 day'));
+                break;
+            case 'next_week':
+                $due_date = date('Y-m-d', strtotime('+1 week'));
+                break;
+            case 'in_2_week':
+                $due_date = date('Y-m-d', strtotime('+2 week'));
+                break;
+            default:
+                // Gérer les cas d'erreur ou par défaut
+                $_SESSION['message_error'] = "Veuillez choisir une date ! ";
+                header('Location: accueil.php');
+                exit;
+        }
+    }
+
+    // Rediriger vers la page d'accueil en cas d'erreur
+    if (!isset ($due_date)) {
+        header('Location: accueil.php');
+        exit;
+    }
+
     try {
-        // Préparer la requête pour mettre à jour la tâche dans la base de données
-        $stmt = $conn->prepare("UPDATE tasks SET title = :title, due_date = :due_date WHERE id = :id");
-        $stmt->bindParam(':title', $_POST['title']);
-        $stmt->bindParam(':due_date', $_POST['due_date']);
-        $stmt->bindParam(':id', $id_tache);
-
-        // Exécuter la requête de mise à jour
+        // Insérez la tâche dans la base de données avec la priorité spécifiée
+        $stmt = $conn->prepare("INSERT INTO tasks (user_id, title, due_date, priority) VALUES (:user_id, :title, :due_date, :priority)");
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->bindParam(':title', $titre); // Correction de la variable ici
+        $stmt->bindParam(':due_date', $due_date);
+        $stmt->bindParam(':priority', $priority);
         $stmt->execute();
 
         // Rediriger vers la page d'accueil avec un message de succès
-        $_SESSION['message_success'] = "La tâche a été modifiée avec succès.";
-        header('Location: accueil.php');
+        $_SESSION['message_success'] = "La tâche a été ajoutée avec succès.";
+        header("Location: accueil.php");
         exit;
     } catch (PDOException $e) {
-        // En cas d'erreur, afficher un message d'erreur
-        $_SESSION['message_error'] = "Erreur lors de la modification de la tâche : " . $e->getMessage();
-        // Rediriger vers la page d'accueil
-        header('Location: accueil.php');
+        // En cas d'erreur lors de l'insertion dans la base de données
+        $_SESSION['message_error'] = "Erreur lors de l'ajout de la tâche : " . $e->getMessage();
+        header("Location: accueil.php");
         exit;
     }
+} else {
+    // Si tous les champs requis ne sont pas soumis, redirigez avec un message d'erreur
+    $_SESSION['message_error'] = "Veuillez remplir tous les champs.";
+    header("Location: accueil.php");
+    exit;
 }
-
-$pageTitle = "Modifier la tâche";
-require_once "header.php";
 ?>
-
-<body>
-    <div class="container">
-        <h1>Modifier la tâche</h1>
-        <form action="modification_tache.php" method="post">
-            <!-- Champ caché pour envoyer l'ID de la tâche -->
-            <input type="hidden" name="id" value="<?php echo $id_tache; ?>">
-            <div class="mb-3">
-                <label for="title" class="form-label">Titre de la tâche</label>
-                <input type="text" class="form-control" id="title" name="title" value="<?php echo $tache['title']; ?>">
-            </div>
-            <div class="mb-3">
-                <label for="due_date" class="form-label">Date d'échéance</label>
-                <input type="date" class="form-control" id="due_date" name="due_date"
-                    value="<?php echo $tache['due_date']; ?>">
-            </div>
-            <button type="submit" name="modifier_tache" class="btn btn-primary">Modifier la tâche</button>
-        </form>
-    </div>
-</body>
-
-</html>
