e037909f40a2c17298d29975ca7f87d64e5ad4a3
Rayane <rchaabane20@gmail.com>
2024-03-21 17:16:05 +0100

    Sign In Form Page

--- php/sign_in.php
+++ php/sign_in.php
@@ -51,6 +51,7 @@
                                 </div>
                             </div>
 
+
                             <form id="login_Form" method="POST" action="/php/signIn_processing.php">
                                 <!-- Assurez-vous que le formulaire pointe vers la bonne page PHP -->
                                 <div class="form-group">
@@ -89,7 +90,7 @@
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-        </script>
+    </script>
     <script src="../js/script.js"></script>
 </body>
 
