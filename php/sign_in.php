50ed23a52c0912942c4d216f6a68fe904d44e910
Rayane <rchaabane20@gmail.com>
2024-03-23 02:25:57 +0100

    Sign in Form

--- php/sign_in.php
+++ php/sign_in.php
@@ -71,8 +71,9 @@
                                         <label class="form-check-label rememberMe-label" for="rememberMe">Se
                                             Rappeler de moi</label>
                                     </div>
-                                    <!-- <div class="link forget-pass"><a href="forgot_password.php">Mot de passe
-                                            oublié?</a></div> -->
+                                    <div class="link forget-pass"><a href="forgot_password.php">Mot de passe oublié?</a>
+                                    </div>
+
                                 </div>
                                 <div class="button-group">
                                     <button type="submit" class="btn btn-primary btn-lg">Connexion</button>
@@ -90,7 +91,7 @@
     </section>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
-        </script>
+    </script>
     <script src="../js/script.js"></script>
 </body>
 
